3b95302be900b08b3c1daf5b9f39eae9171633f0
Prathamesh Karatkar <117072460+prathameshkaratkar@users.noreply.github.com>
2024-01-20 06:37:00 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS as newer version
+
+
+//alert( 3 + 3 ) // we are using nodejs, not browser
+
+// code readability should be high
+
+let name = "prathamesh"
+let age = 18
+let isLoggedIn = false
+let state;
+ 
+// number => 2 to power of 53
+// bigint  (stock market values, reddit, instagram post)
+// string => ""
+//Boolean => true /false 
+// null => (standalone value) it is representation of empty value
+// undefined => yet value is not defined
+//symbol => unique
+
+// object 
+
+console.log(typeof null); //object
+console.log(typeof undefined);  // undefined
